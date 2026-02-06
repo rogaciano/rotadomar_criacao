a869fdc5d51899fabf596b428facb25125b94676
rogaciano <rogaciano@gmail.com>
2026-02-06 15:35:34 -0300

    feat: Add dark mode styling to application logo with wrapper and background contrast

--- windsurf/resources/views/components/application-logo.blade.php
+++ windsurf/resources/views/components/application-logo.blade.php
@@ -1,1 +1,15 @@
-﻿<img src="{{ asset('images/rota_logo.png') }}" {{ $attributes }} alt="Rota do Mar">
+﻿<span class="logo-wrapper">
+    <img src="{{ asset('images/rota_logo.png') }}" {{ $attributes }} alt="Rota do Mar">
+</span>
+<style>
+    .dark .logo-wrapper {
+        background-color: rgba(255, 255, 255, 0.9);
+        border-radius: 0.5rem;
+        padding: 0.25rem 0.75rem;
+        display: inline-block;
+    }
+    .logo-wrapper {
+        display: inline-block;
+        transition: all 0.3s;
+    }
+</style>
